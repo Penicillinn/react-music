e71294a697344a4cdf9a4f9b666905e54a2b0574
庄佳佳 <zjj@zhuangjiajiadeMacBook-Pro.local>
2020-08-09 19:12:45 +0800

    fix

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,4 @@
-const devBaseURL = "http://123.207.32.32:9001";
+const devBaseURL = "https://dev.org";
 const proBaseURL = "https://production.org";
 export const BASE_URL = process.env.NODE_ENV === 'development' ? devBaseURL: proBaseURL;
 
